e71a399ced47819538d4babf0ca074e3efe838fe
Noah Holloway <noahholloway23@gmail.com>
2025-02-02 19:17:42 -0700

    Add project files.

---
+++ BetterCalculator/My Project/AssemblyInfo.vb
@@ -0,0 +1,32 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("BetterCalculator")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("Razer")>
+<Assembly: AssemblyProduct("BetterCalculator")>
+<Assembly: AssemblyCopyright("Copyright © Razer 2025")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("e86c017d-dc33-41d3-bfc1-41381c1aa969")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
